5e4c4b78616bf80df588fdad80c8951735721863
Rblashchuk <rblashchuk@gmail.com>
2019-12-13 17:32:12 +0300

    Update formatting_text.py
    Deleting of trash words was added (see trash.txt)
    Take note that this list may be different for different aims
    Code was simplified and rewrote to make it more readable (I got rid of SpaCy dependencies and divided it into functions)
    
    Remaining problem is to fix mistakingly taken normal forms (like "висок" as a normal form for "виски")

--- formatting_text.py
+++ formatting_text.py
@@ -1,43 +1,81 @@
 import re
-from pymongo import MongoClient
-import spacy
 import pymorphy2
-mclient = MongoClient('localhost', 27017)
-db = mclient.restotexts
-posts = db.posts
+from pymongo import MongoClient
 
-nlp = spacy.load('ru2')
 morph = pymorphy2.MorphAnalyzer()
 
-for post in posts.find():
-    
-    text = post.get('text')
-    text = re.sub(r"\n+", " ", text)
-    text = re.sub(r"[^A-Za-zА-Яа-я0-9Ёё.]+", '', text)
-    text = text.lower()
-    
-    doc = nlp(text)
-    l = [word.lemma_ for word in doc]
-    for word in l:
-        p = morph.parse(word)[0]
-        t = p.tag
-        i = l.index(word)
-        
-        if 'NPRO' in t\
-                or 'PRED' in t\
-                or 'PREP' in t\
-                or 'CONJ' in t\
-                or 'PRCL' in t\
-                or 'INTJ' in t\
-                or 'Apro' in t\
-                or 'Ques' in t\
-                or 'Prdx' in t\
-                or 'Dmns' in t:
-            l.pop(i)
-            
-    text = ' '.join([word for word in l])
-    
-    text = re.sub(" +", " ", text)
-    
+mclient = MongoClient('localhost', 27017)
+db = mclient.restotexts
+mposts = db.posts
+
+
+def norm_word(word):
+    return morph.parse(word)[0].normal_form
+
+
+def trashpos(word):
+    tag = morph.parse(word)[0].tag
+    if 'NPRO' in tag\
+            or 'PRED' in tag\
+            or 'PREP' in tag\
+            or 'CONJ' in tag\
+            or 'PRCL' in tag\
+            or 'INTJ' in tag\
+            or 'Apro' in tag\
+            or 'Ques' in tag\
+            or 'Prdx' in tag\
+            or 'NUMR' in tag\
+            or 'Dmns' in tag:
+        return True
+    else:
+        return False
+
+
+def format(text):
+    text = re.sub(r"[^А-Яа-яЁё ]+", ' ', text)
+    return text.lower()
+
+
+def del_spaces(text):
+    while "  " in text:
+        text = text.replace("  ", " ")
+    return text
+
+
+def text2list(text):
+    text = format(text)
+    list = text.split(' ')
+    return list
+
+
+def list2text(list):
+    text = ' '.join([word for word in list])
+    return text
+
+
+file_trash = open('trash.txt', 'r')
+trash = text2list(file_trash.read())
+
+for post in mposts.find():
     id = post.get("_id")
+    text = post.get('text')
+    if text == "" or text is None:
+        continue
+
+    text = format(text)
+    l = text2list(text)
+    l = [norm_word(word) for word in l]
+
+    to_delete = []
+    for index, word in enumerate(l):
+        if trashpos(word) or norm_word(word) in trash:
+            to_delete.append(index)
+            print(word.upper(), l)
+
+    for index in sorted(to_delete, reverse=True):
+        del l[index]
+
+    text = del_spaces(list2text(l))
     db.posts.update_one({"_id": id}, {"$set": {"formatted_text": text}})
+
+file_trash.close()
